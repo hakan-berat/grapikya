43ddbdb8eee725b99781199ffb5902d0d11b9e28
Hakan Berat <grapikya@gmail.com>
2023-03-15 12:48:33 +0300

    Responsive device size with media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,26 @@ navLinks.forEach(element => {
     element.addEventListener('click', closeNav)
 })
 
+/*HEADER BACKGROUND WHEN SCROLL*/
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50){
+        header.classList.add('bg-header')
+
+        navLinks.forEach(link => {
+            link.classList.add('nav-link-shadow')
+        })
+    }
+    else{
+        header.classList.remove('bg-header')
+
+        navLinks.forEach(link => {
+            link.classList.remove('nav-link-shadow')
+        })
+    }
+})
+
 /*SWIPPER*/
 
 let swiper = new Swiper('.swiper-container', {
@@ -44,8 +64,13 @@ let swiper = new Swiper('.swiper-container', {
 
     breakpoints: {
         // when window width is >= 320px
-    567: {
+        567: {
             slidesPerView: 2,
+            spaceBetween: 10,
+        },
+
+        885: {
+            slidesPerView: 3,
             spaceBetween: 10,
         },
     },
